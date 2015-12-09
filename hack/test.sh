36ffc0aa97fc7fb3601f3f01e93732cdb60027ea
Jessica Frazelle <acidburn@docker.com>
2015-12-08 17:07:00 -0800

    add downloading frozen images to integration test
    Signed-off-by: Jessica Frazelle <acidburn@docker.com>

--- hack/test.sh
+++ hack/test.sh
@@ -4,6 +4,7 @@ set -e
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 source "${DIR}/.integration-daemon-start"
+source "${DIR}/.ensure-frozen-images"
 source "${DIR}/.onion-start"
 
 # run the tor router
